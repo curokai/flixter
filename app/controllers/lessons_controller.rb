caf3d2a29c484ae81e1a5783de485241cfa41730
curokai <anadan20182019@gmail.com>
2019-07-26 20:20:47 +0000

    flx29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,28 @@
 class LessonsController < ApplicationController
+   before_action :authenticate_user!
+   before_action :require_authorized_for_current_course, only: [:show]
+
    def show
     @lesson=Lesson.find(params[:id])
   end
+
+   
+
+  private
+
+    def require_authorized_for_current_course
+    if current_course.user != current_user
+      flash[:alert] = "You have to be enrolled to this course to view lessons!"
+       redirect_to  course_path
+    
+   end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+
+
 end
