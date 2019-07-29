89b981d10bc948ebb9eeb66be1a671c3e4a11c31
curokai <anadan20182019@gmail.com>
2019-07-29 14:21:49 +0000

    Delete Section

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-   resource :dashboard, only: [:show]
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  resources :lessons, only: [:show] 
-
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:update]
-      resources :sections, only: [:new, :create]
+     
     end
   end
- resources :charges
-
-end 
+end
