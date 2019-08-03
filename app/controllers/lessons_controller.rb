e791945f7bb8ab62eabe19eff53b557a49c3a4c2
curokai <anadan20182019@gmail.com>
2019-08-03 01:52:13 +0000

    LastFlixter

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,8 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course], only: [:show]
+
+  
   def show
   end
 
@@ -8,4 +12,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_course
+    if current_user.enrolled_in?
+    else
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
 end
